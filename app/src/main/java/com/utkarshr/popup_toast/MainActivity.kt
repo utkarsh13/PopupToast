6e8c371b6ac9afd66d8e4e074e9d3bd4cd9f486b
Utkarsh Rastogi <utkarsh.013@gmail.com>
2020-05-25 16:54:55 +0530

    Adding touch listener to drag view

--- app/src/main/java/com/utkarshr/popup_toast/MainActivity.kt
+++ app/src/main/java/com/utkarshr/popup_toast/MainActivity.kt
@@ -6,21 +6,28 @@ import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.OvershootInterpolator
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.utkarshr.popup_toast.Utils.Companion.ifLet
 
 
-class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
+class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener, View.OnTouchListener {
 
     var mView: View? = null
 
     var mRootViewGroup: ViewGroup? = null
+
+    private var mTopMargin = 0
+    private var mViewY = 0
+    private var mYDelta = 0
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,9 +57,16 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
         mView?.y = Utils.dpToPx(200).toFloat()
 
         mView?.addOnLayoutChangeListener(this)
+        mView?.setOnTouchListener(this)
 
         mView?.visibility = View.INVISIBLE
-        mRootViewGroup?.addView(mView, LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT))
+        mRootViewGroup?.addView(
+            mView,
+            LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        )
     }
 
     private fun removeView() {
@@ -65,12 +79,15 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
             anim.addListener(object : Animator.AnimatorListener {
                 override fun onAnimationRepeat(p0: Animator?) {
                 }
+
                 override fun onAnimationEnd(p0: Animator?) {
                     mRootViewGroup?.removeView(it)
                     mView = null
                 }
+
                 override fun onAnimationCancel(p0: Animator?) {
                 }
+
                 override fun onAnimationStart(p0: Animator?) {
                 }
             })
@@ -78,7 +95,7 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
     }
 
     override fun onLayoutChange(
-        view: View,
+        view: View?,
         left: Int,
         top: Int,
         right: Int,
@@ -88,14 +105,17 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
         oldRight: Int,
         oldBottom: Int
     ) {
-        view.visibility = View.VISIBLE
-        val viewWidth = view.width
-        val viewHeight = view.height
-        val newX = (Utils.screenWidth - viewWidth)/2
+        view?.visibility = View.VISIBLE
+        val viewWidth = view?.width ?: 0
+        val viewHeight = view?.height ?: 0
+        val newX = (Utils.screenWidth - viewWidth) / 2
         val startY = mRootViewGroup!!.height
         val finalY = mRootViewGroup!!.height - viewHeight - Utils.dpToPx(24)
-        view.x = newX.toFloat()
-        view.y = startY.toFloat()
+        view?.x = newX.toFloat()
+        view?.y = startY.toFloat()
+
+        mTopMargin = (view?.layoutParams as FrameLayout.LayoutParams).topMargin
+        mViewY = finalY
 
         val anim = ObjectAnimator.ofFloat(view, "translationY", finalY.toFloat())
         anim.duration = 300
@@ -103,6 +123,36 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener {
         anim.start()
 
         view.removeOnLayoutChangeListener(this)
+    }
+
+    override fun onTouch(view: View?, event: MotionEvent?): Boolean {
+        ifLet(view, event, mRootViewGroup) {
+
+            val y = event!!.rawY.toInt()
+            val layoutParams = view!!.layoutParams as FrameLayout.LayoutParams
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_DOWN -> {
+                    mYDelta = y - layoutParams.topMargin
+                }
+                MotionEvent.ACTION_UP -> {
+                    if (layoutParams.topMargin - mTopMargin > Utils.dpToPx(24)) {
+                        removeView()
+                    } else {
+
+                    }
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    val newTopMargin: Int = if ((y - mYDelta) < mTopMargin) mTopMargin else (y - mYDelta)
+                    layoutParams.topMargin = newTopMargin
+                    layoutParams.bottomMargin = -250
+                    view.layoutParams = layoutParams
+                }
+                else -> {
+                }
+            }
+            mRootViewGroup!!.invalidate()
+        }
+        return true
     }
 
 }
