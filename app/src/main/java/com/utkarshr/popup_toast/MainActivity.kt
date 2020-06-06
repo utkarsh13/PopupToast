4cb30243fe3a939d9cf38b1e479a9a23ee51fbfa
Utkarsh Rastogi <utkarsh.013@gmail.com>
2020-06-07 00:04:54 +0530

    Moving enum to different class

--- app/src/main/java/com/utkarshr/popup_toast/MainActivity.kt
+++ app/src/main/java/com/utkarshr/popup_toast/MainActivity.kt
@@ -265,9 +265,3 @@ class MainActivity : AppCompatActivity(), View.OnLayoutChangeListener, View.OnTo
     }
 }
 
-enum class ToastHandlerMessage(val value: Int) {
-    MSG_SHOW(1),
-    MSG_HIDE(2),
-    MSG_REMOVE_TIMER(3),
-    MSG_ADD_TIMER(4)
-}
